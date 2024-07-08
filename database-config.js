25f9bba579af70290c582c9119bf6e2f49c6a88e
alreem <alreemmaskari@gmail.com>
2024-07-08 13:56:06 +0400

    Initial commit

--- database-config.js
+++ database-config.js
@@ -2,12 +2,12 @@
 const sql = require("mssql")
 const { serve } = require("swagger-ui-express")
 const config = {
-    user: 'sa',
-    password: 'sa',
-    server: 'localhost',
+    user: 'adminalreem',
+    password: '97919895@Reen',
+    server: 'serveruser.database.windows.net',
     database: 'Shopping_DB', 
     options:{
-        trustServerCertificate:true
+        encrypt:true
     }
 };
 
